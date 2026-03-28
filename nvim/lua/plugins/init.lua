750d8b4597602c5ff6962f65890e0239c1fe9dbc
pokemans123 <pranavarunkumar2010@gmail.com>
2026-03-27 20:45:23 -0400

    regular commit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -94,4 +94,13 @@ return {
       require("telescope").load_extension "ui-select"
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
